bbc9aaf76fadcccc895f3b2fda28b8c7a6b57b25
katieflux <kmbpzlg7145@hotmail.com>
2025-10-21 22:36:28 +0800

    remove token owner

--- contracts/ERC7984Token.sol
+++ contracts/ERC7984Token.sol
@@ -6,35 +6,12 @@ import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
 
 contract ERC7984Token is ERC7984, SepoliaConfig {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    error NotOwner();
     error ZeroAddress();
 
-    modifier onlyOwner() {
-        if (msg.sender != _owner) revert NotOwner();
-        _;
-    }
+    constructor() ERC7984("TEST", "TEST", "") {}
 
-    constructor() ERC7984("TEST", "TEST", "") {
-        _owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
-    }
-
-    function owner() external view returns (address) {
-        return _owner;
-    }
-
-    function transferOwnership(address newOwner) external onlyOwner {
-        if (newOwner == address(0)) revert ZeroAddress();
-        address previousOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(previousOwner, newOwner);
-    }
-
-    function mint(address to, uint64 amount) external onlyOwner {
+    function mint(address to, uint64 amount) external {
+        if (to == address(0)) revert ZeroAddress();
         euint64 encryptedAmount = FHE.asEuint64(amount);
         _mint(to, encryptedAmount);
     }
